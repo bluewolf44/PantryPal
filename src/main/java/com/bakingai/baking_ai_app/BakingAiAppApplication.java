d41c2bc39d8e02079441be5fde0011094d328a24
glen <gargl791@student.otago.ac.nz>
2024-07-23 14:07:26 +1200

    initialize springboot with dependencies for spring security, postgres db, Spring JPA etc.

---
+++ src/main/java/com/bakingai/baking_ai_app/BakingAiAppApplication.java
@@ -0,0 +1,13 @@
+package com.bakingai.baking_ai_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakingAiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakingAiAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bakingai/baking_ai_app/BakingAiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bakingai.baking_ai_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakingAiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
