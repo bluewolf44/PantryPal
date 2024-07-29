83e8d4d40c86d060a82f8e10c98390d280cc6357
bluewolf44 <danielwest618@gmail.com>
2024-07-30 11:46:48 +1200

    register the models to run inside the admin
    you need to run "python manage.py createsuperuser" to create a user to access the admin page

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models  import *
+
+admin.site.register(User)
+admin.site.register(Ingredient)
+admin.site.register(Recipe)
+admin.site.register(Required)
