a2dfd9983969a37039e18fb8805e0383445ec0dc
bluewolf44 <danielwest618@gmail.com>
2024-07-30 11:51:36 +1200

    Setting toString from the models

--- main/models.py
+++ main/models.py
@@ -9,19 +9,27 @@ class User(models.Model):
 	firstname = models.CharField(max_length=50)
 	lastname = models.CharField(max_length=50)
 	email = models.CharField(max_length=100)
+	def __str__(self):
+		return self.username
 
 class Ingredient(models.Model):
 	ingredientName = models.CharField(max_length=50)
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	amount = models.IntegerField()
 	describe = models.TextField()
+	def __str__(self):
+		return self.ingredientName +" by " + self.user.username
 
 class Recipe(models.Model):
 	recipeName = models.CharField(max_length=50)
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	recipe = models.TextField()
+	def __str__(self):
+		return self.recipeName +" by " + self.user.username
 
 class Required(models.Model):
 	recipe = models.ForeignKey(Recipe, on_delete = models.CASCADE)
 	ingredient = models.ForeignKey(Ingredient, on_delete = models.CASCADE)
 	amount = models.IntegerField()
+	def __str__(self):
+		return self.ingredient.ingredientName + " in "+  self.recipe.recipeName +" by " + self.user.username
