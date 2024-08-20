51c26a15d67ba883a0a08d2d7a20ec667a7e83b9
phiha428 <phiha428@student.otago.ac.nz>
2024-08-20 20:57:00 +1200

    started writing tests for models

---
+++ main/tests/factories.py
@@ -0,0 +1,25 @@
+from pickle import FALSE
+
+import factory
+from django.contrib.auth.models import User
+from ..models import Ingredient
+
+# Not sure if we have a User setup or whether this is done separately in django
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+    password = "test"
+    username = "test"
+    is_superuser = True
+    is_staff = True
+
+class IngredientFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Ingredient
+
+    ingredeintName = "Cheese"
+
+    amount = 2
+    describe = "I have two slices of cheese"
+    liquid = FALSE
+
