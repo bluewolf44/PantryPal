67c0021d2d8b8227038aa69ce957cf0ef78035fc
gargl791 <glenmarcogarcia123@gmail.com>
2024-09-18 17:14:11 +1200

    update saverecipes.jsx

--- frontend/src/recipes.jsx
+++ frontend/src/recipes.jsx
@@ -2,9 +2,14 @@ import React, { useState,useEffect  } from 'react';
 import Modal from 'react-modal';
 import './css/saverecipe.css';  // Assuming your CSS is adapted for React
 
+axios.defaults.xsrfCookieName = 'csrftoken';
+axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+axios.defaults.withCredentials = true;
+
+
 Modal.setAppElement('#root');  // Assuming your root div has an ID of 'root'
 
-function SaveRecipe() {
+function RecipeGrid() {
  
 
     return (
