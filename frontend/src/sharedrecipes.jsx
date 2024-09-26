cb25dd6d69571b00033bb3bac29f6cf9b87377c2
gargl791 <glenmarcogarcia123@gmail.com>
2024-09-26 21:57:59 +1200

    update sharedrecipes.jsx

--- frontend/src/sharedrecipes.jsx
+++ frontend/src/sharedrecipes.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import Modal from "react-modal";
 import axios from "axios";
 import RecipeDetailsModal from "./modals/RecipeDetailsModal";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 import "./css/saverecipe.css"; // Assuming your CSS is adapted for React
 
@@ -14,6 +14,7 @@ Modal.setAppElement("#root"); // Assuming your root div has an ID of 'root'
 
 function SharedRecipesGrid() {
   const [recipesReceived, setRecipesReceived] = useState([])
+  const [recipesShared, setRecipesShared] = useState([])
 
   useEffect(() => {
     // This fetch will contain a custom json including recipeOwner, userShared, recipe, and feedback
@@ -23,12 +24,28 @@ function SharedRecipesGrid() {
         setRecipesReceived(response.data);
         console.log("Recipes Received: ", recipesReceived)
         console.log("response.data: ", response.data)
+        console.log("test: ", response.data[0].recipeName)
+        console.log("test: ", response.data[0].recipeName.recipeName)
+
       } catch (error) {
         console.log("Error in getting recipes from backend: ", error);
       }
     };
     getRecipesReceived();
-  }, [])
+  }, [useLocation()])
+
+  // useEffect(() => {
+  //   const getRecipesShared = async () => {
+  //     try {
+  //       const response = await axios.get("/api/getRecipesShared/");
+  //       setRecipesShared(response.data);
+  //       console.log("response.data: ", response.data)
+  //     } catch (error) {
+  //       console.log("Error in getting recipes shared from backend: ", error);
+  //     }
+  //   };
+  //   getRecipesShared();
+  // }, [useLocation()])
 
   return (
     <>
@@ -38,14 +55,15 @@ function SharedRecipesGrid() {
           recipesReceived.map((shared) => (
             <div key={shared.pk} className="recipes">
               <img
-                src={"Storage/" + shared.recipeName.recipeName}
-                alt={shared.fields.recipeName}
+                src={"Storage/" + shared.recipeName.picture}
+                alt={shared.recipeName.recipeName}
             />
-              <span>{shared.fields.recipeName}</span>
+              <span>{shared.recipeName.recipeName}</span>
+              <span>{"Shared by " + shared.recipeOwner.username}</span>
               {/* <span>{recipe.fields.recipe}</span> */}
               <div className="recipes-buttons">
-                <button>View Details</button>
-                <button>Edit</button>
+                <button>Add to your recipes</button>
+                <button>Give Feedback</button>
                 <button>Delete</button>
               </div>
 
@@ -54,9 +72,6 @@ function SharedRecipesGrid() {
       ) : (
         <p>No Recipes Found</p>
       )}
-      </div>
-      <h2> Recipes I shared: </h2>
-      <div className="recipes-grid">
       </div>
     </>
   );
