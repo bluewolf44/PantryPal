d35ea5a0d827218a0dfc31dc2907b3e3a851d2b4
phiha428 <phiha428@student.otago.ac.nz>
2024-09-26 16:19:17 +1200

    added ios related files to the repo

---
+++ pantryPal-ios/PantryPalApp/PantryPalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PantryPalAppApp.swift
+//  PantryPalApp
+//
+//  Created by Hamish Phillips on 24/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct PantryPalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
